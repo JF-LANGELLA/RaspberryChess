08a1a26a5347e8cbbcac33147d7e1c626890c1cc
AmmarFitriAmin <50488909+AmmarFitriAmin@users.noreply.github.com>
2019-05-26 17:33:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var port = process.env.PORT;
+var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
